d9866b7f7a18d31b8abb2e498a344a4c5d5dbe8a
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2016-04-28 12:36:13 +0300

    Add one more test

--- src/test/java/com/fmdev/tdd/AlarmTest.java
+++ src/test/java/com/fmdev/tdd/AlarmTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.*;
 public class AlarmTest {
     private static final double GOOD = 20.0;
     private static final double TOO_LOW = 14.0;
+    private static final double TOO_HIGH = 26.0;
     private Alarm alarm;
     private double pressure;
 
@@ -26,6 +27,12 @@ public class AlarmTest {
     @Test
     public void beOnWhenPressureIsTooLow() {
         checkAlarmWhenPressureIs(TOO_LOW);
+        assertThat(alarm.isAlarmOn(), is(true));
+    }
+    
+    @Test
+    public void beOnWhenPressureIsTooHigh() {
+        checkAlarmWhenPressureIs(TOO_HIGH);
         assertThat(alarm.isAlarmOn(), is(true));
     }
 
